419bbf312c6fdf48bd07b99de2b38ddeb5670026
Iman2244 <152248162+Iman2244@users.noreply.github.com>
2024-01-15 22:53:13 +0800

    Update UserClass.java
    button for user class

--- UserClass.java
+++ UserClass.java
@@ -5,49 +5,113 @@
  */
 package oop2project;
 
-/**
- *
- * @author iman bakour
- */
-public class UserClass {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
-  // Instance variables
-  private String username;
-  private String password;
-  
-  // Constructor
-  public UserClass(String username, String password) {
-    this.username = username;
-    this.password = password;
-  }
+public class UserClass extends Application {
 
-  // Getter and setter methods
-  public String getUsername() {
-    return username; 
-  }
+    private String adminUsername = "admin";
+    private String adminPassword = "admin";
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-  public String getPassword() {
-    return password;
-  }
-  
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Login");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
 
-  // Login method
-  public boolean login(String username, String password) {
-    // Check entered username & password against 
-    // stored credentials
-    return true; 
-  }
-  
-  // Register new user method
-  public void registerUser(String username, String password) {
-    // Store new username & password 
-  }
-  
+        Text sceneTitle = new Text("Login");
+        sceneTitle.setTextAlignment(TextAlignment.CENTER);
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Username");
+        userName.setFont(Font.font("Tahoma", FontWeight.BOLD, 14));
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        pw.setFont(Font.font("Tahoma", FontWeight.BOLD, 14)); 
+        grid.add(pw, 0, 2);
+
+        PasswordField passwordBox = new PasswordField();
+        grid.add(passwordBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_LEFT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                String username = userTextField.getText().toString();
+                String password = passwordBox.getText().toString();
+
+                if(userTextField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, "Form Error!",
+                            "Please enter your email id");
+                    return;
+                }
+                if(passwordBox.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, "Form Error!",
+                            "Please enter a password");
+                    return;
+                }
+
+                if(adminUsername.equals(username) && adminPassword.equals(password)){
+                    infoBox("Login Successful!", null, "Success");
+                } else{
+                    infoBox("Please enter correct Email and Password", null, "Failed");
+                }
+            }
+        });
+
+        Scene scene = new Scene(grid, 400, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private static void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
+    public static void infoBox(String infoMessage, String headerText, String title){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
 }
+
