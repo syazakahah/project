2c3b65dcddd1f6a1bf64a6a3aea978b9b3a7f27a
syazakahah <156535764+syazakahah@users.noreply.github.com>
2024-01-16 00:19:04 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,4 +1,4 @@
-package org.openjfx.oop_project;
+package org.openjfx.homeinventoryproject;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -13,14 +13,11 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+    InventoryController inventorycontrol=new InventoryController();
+    primaryStage.setTitle("HomeInventoryManager");
+    primaryStage.setScene(inventorycontrol.getScene());
+    primaryStage.show();
     }
 
     public static void main(String[] args) {
