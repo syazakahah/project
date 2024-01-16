c5fd28fa794978653e1ce05e89389260b7f51590
eirdyana <139585616+eirdyana@users.noreply.github.com>
2024-01-17 02:49:26 +0800

    Create LogInyana.java
    log in class (yana's version)

---
+++ LogInyana.java
@@ -0,0 +1,122 @@
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets; 
+import javafx.geometry.Pos; 
+import javafx.scene.Scene; 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button; 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField; 
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage; 
+
+public class LogIn {
+    private String adminUsername = "oop";
+    private String adminPassword = "oop123";
+    private Scene sceneLogin;
+    private homepage homepage;
+    private Stage primaryStage;
+    
+    public LogIn(homepage homepage, Stage primaryStage){
+        this.homepage = homepage;
+        //this.primaryStage = primaryStage; 
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25)); 
+        
+        Label sceneTitle = new Label("Login");
+        sceneTitle.setTextAlignment(TextAlignment.CENTER);
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+        
+        Label usn = new Label("Username");       
+        Label pass = new Label("Password"); 
+       
+        VBox first = new VBox(10);
+        first.getChildren().addAll(usn, pass);
+        first.setAlignment(Pos.CENTER);
+
+        TextField usnText = new TextField();       
+        TextField passText = new TextField();  
+       
+        VBox second = new VBox(10);
+        second.getChildren().addAll(usnText, passText);
+        second.setAlignment(Pos.CENTER);
+       
+        HBox container1 = new HBox(10);
+        container1.getChildren().addAll(first, second);
+        container1.setAlignment(Pos.CENTER);  
+        
+        Button submit = new Button("Submit"); 
+        Button back = new Button("Back"); 
+       
+        HBox buttons = new HBox(10);
+        buttons.getChildren().addAll(submit, back);
+        buttons.setAlignment(Pos.CENTER);  
+      
+        submit.setOnAction((ActionEvent e) -> {
+            String username = usnText.getText().toString();
+            String password = passText.getText().toString();
+          
+            if(usnText.getText().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, "Form Error!",
+                        "Please enter your username");
+                return;
+            }
+            if(passText.getText().isEmpty()) {
+                showAlert(Alert.AlertType.ERROR, "Form Error!",
+                        "Please enter a password");
+                return;
+            }
+          
+            if(adminUsername.equals(username) && adminPassword.equals(password)){
+                infoBox("Login Successful!", null, "Success");
+            } else{
+                infoBox("Please enter correct Username and Password", null, "Failed");
+            }
+        });
+      
+        back.setOnAction((ActionEvent e) -> {
+            homepage.switchToHomepage();
+        });
+      
+        VBox root = new VBox(5);
+        root.getChildren().addAll(sceneTitle, container1, buttons);
+        root.setAlignment(Pos.CENTER);
+        root.setBorder(new Border(new BorderStroke(Color.PINK, BorderStrokeStyle.DASHED, new CornerRadii(10), new BorderWidths(3)))); 
+    
+        sceneLogin = new Scene(root, 600, 400);
+    }
+    
+    public Scene getScene() {
+        return sceneLogin;
+    }
+        
+    private static void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
+    public static void infoBox(String infoMessage, String headerText, String title){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    } 
+}
